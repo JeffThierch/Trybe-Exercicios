564a2517071c89ee6b702bff361581c6c66cc920
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2021-08-25 15:36:28 -0300

    Reatribuicao da Constante birthCity

--- Fundamentos/Bloco-4-Introducao-JavaScript-e-Logica-Programacao/Dia-1-JavaScript-Primeiros-Passos/Exercicio-Fixacao-01/script.js
+++ Fundamentos/Bloco-4-Introducao-JavaScript-e-Logica-Programacao/Dia-1-JavaScript-Primeiros-Passos/Exercicio-Fixacao-01/script.js
@@ -4,6 +4,10 @@ let birthYear = 2000;
 
 birthYear = 2030
 
+birthCity = "Sao Paulo"
+
 console.log(myName, birthCity, birthYear)
 
-console.log(birthYear)
+console.log(birthYear);
+
+console.log(birthCity) /* Deu o erro porque Constantes nao podem ser reatribuidas */
