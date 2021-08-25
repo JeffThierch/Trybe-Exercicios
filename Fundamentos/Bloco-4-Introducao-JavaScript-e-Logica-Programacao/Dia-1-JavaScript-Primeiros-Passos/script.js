3052de99f91da8f75e1bbe30bab109e0fb713200
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2021-08-25 19:37:25 -0300

    Programa 08 - verifica se e positivo/negativo/zero

--- Fundamentos/Bloco-4-Introducao-JavaScript-e-Logica-Programacao/Dia-1-JavaScript-Primeiros-Passos/script.js
+++ Fundamentos/Bloco-4-Introducao-JavaScript-e-Logica-Programacao/Dia-1-JavaScript-Primeiros-Passos/script.js
@@ -47,3 +47,15 @@ if(numero1 > numero2 && numero1 > numero3){
   console.log("O Numero: " + numero3 + " e maior" )
 }
 
+console.log("Programa 08")
+
+const numeroDado = 0
+
+if(numeroDado < 0){
+  console.log("negative")
+}else if(numeroDado > 0){
+  console.log("positive")
+}else{
+  console.log("zero")
+}
+
