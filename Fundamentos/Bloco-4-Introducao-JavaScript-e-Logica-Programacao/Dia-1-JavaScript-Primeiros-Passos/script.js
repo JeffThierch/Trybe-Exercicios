fa0b29e0ffc8480558f8b8320150ef676ca4e04f
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2021-08-25 19:32:47 -0300

    Programa 06 e 7

--- Fundamentos/Bloco-4-Introducao-JavaScript-e-Logica-Programacao/Dia-1-JavaScript-Primeiros-Passos/script.js
+++ Fundamentos/Bloco-4-Introducao-JavaScript-e-Logica-Programacao/Dia-1-JavaScript-Primeiros-Passos/script.js
@@ -1,21 +1,49 @@
 const a = 30;
 const b = 20;
 
-let adicao = a + b
+let adicao = a + b;
 
-console.log(adicao)
+console.log(adicao);
 
 let subtracao = a - b;
 
 console.log(subtracao);
 
-let multiplicacao = a * b
+let multiplicacao = a * b;
 
-console.log(multiplicacao)
+console.log(multiplicacao);
 
-let divisao = a / b
-console.log(divisao)
+let divisao = a / b;
 
-let modulo = a % b
+console.log(divisao);
 
-console.log(modulo)
+let modulo = a % b;
+
+console.log(modulo);
+
+
+console.log("Programa 06")
+
+const numeroA = 50;
+const NumeroB = 55.5;
+
+if(numeroA > NumeroB){
+  console.log("O maior numero e: " + numeroA);
+}else{
+  console.log("O maior numero e: " + NumeroB);
+};
+
+console.log("Programa 07")
+
+const numero1 = 50;
+const numero2 = 40;
+const numero3 = 30;
+
+if(numero1 > numero2 && numero1 > numero3){
+  console.log("O Numero: " + numero1 + " e maior" )
+}else if(numero2 > numero1 && numero2 > numero3){
+  console.log("O Numero: " + numero2 + " e maior" )
+}else{
+  console.log("O Numero: " + numero3 + " e maior" )
+}
+
