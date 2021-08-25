c0913e76cf151f1180266d7f74ef2a5b492c5681
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2021-08-25 16:26:35 -0300

    Exercicio Fixacao 3 - Completo

---
+++ Fundamentos/Bloco-4-Introducao-JavaScript-e-Logica-Programacao/Dia-1-JavaScript-Primeiros-Passos/Exercicio-Fixacao-03/scripts.js
@@ -0,0 +1,12 @@
+const nota = 61;
+
+if(nota >= 80){
+  console.log("Parabens, voce foi aprovado");
+}
+else if(nota < 80 && nota >= 60){
+  console.log("Você está na nossa lista de espera")
+}
+else{
+  console.log("Você foi reprovada(o)")
+}
+
