b4f874266e11f8a4b120009166e7d9afa4f4027c
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2021-08-26 10:08:37 -0300

    Programa 11 - Conversor de nota

--- Fundamentos/Bloco-4-Introducao-JavaScript-e-Logica-Programacao/Dia-1-JavaScript-Primeiros-Passos/script.js
+++ Fundamentos/Bloco-4-Introducao-JavaScript-e-Logica-Programacao/Dia-1-JavaScript-Primeiros-Passos/script.js
@@ -101,8 +101,41 @@ switch(pecaEscolhida.toLowerCase()){
     break;
   case "peao":
     console.log("Movimenta-se apenas pra frente e somente captura 2 pecas na diagonal");
-    break;  
+    break;
+  default:
+    console.log("insira uma peca valida");
+    break;
 }
+
+console.log("Programa 11")
+
+let notaPorcentagem = 110
+const calculoPorcentagem = (notaPorcentagem / 100) * 10
+const resultado = calculoPorcentagem
+
+if(resultado >= 9 && resultado <= 10){
+  console.log("A")
+}
+else if(resultado >= 8 && resultado < 9){
+  console.log("B")
+}
+else if(resultado >= 7 && resultado < 8){
+  console.log("C")
+}
+else if(resultado >= 6 && resultado < 7){
+  console.log("D")
+}
+else if(resultado >= 5 && resultado < 6){
+  console.log("E")
+}
+else if(resultado < 5 && resultado >= 0){
+  console.log("F")
+}
+else{
+  console.log("Insira uma nota valida")
+}
+
+
 
 
 
