4b48bf966765bc5980b404d850538816a94336d4
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2021-08-30 15:21:39 -0300

    Exercicio 1 parte 4

--- Fundamentos/Bloco-4-Introducao-JavaScript-e-Logica-Programacao/Dia-4-JavaScript-Objetos-e-Funcoes/Exercicios-Fixacao/exercicio1.js
+++ Fundamentos/Bloco-4-Introducao-JavaScript-e-Logica-Programacao/Dia-4-JavaScript-Objetos-e-Funcoes/Exercicios-Fixacao/exercicio1.js
@@ -9,4 +9,6 @@ console.log("A jogadora " + player.name + " " + player.lastName + " possui : " +
 
 player["bestInTheWorld"] = [2006, 2007, 2008, 2009, 2010, 2018]
 
-console.log("A jogadora Marta Silva foi eleita a melhor do mundo por 6 vezes " + player.bestInTheWorld)
+console.log("A jogadora Marta Silva foi eleita a melhor do mundo por " + player.bestInTheWorld.length +  " vezes"  )
+
+console.log("A jogadora possui " + player.medals.golden +  " medalhas de ouro e " + player.medals.silver +  " medalhas de prata" )
