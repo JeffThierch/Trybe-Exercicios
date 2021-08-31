90efe6a872e3161a4a5c22f3faa39b5e595b6072
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2021-08-31 10:58:35 -0300

    Exercicio Pratico 6 - somaNumerosAteNumeroDesejado

---
+++ Fundamentos/Bloco-4-Introducao-JavaScript-e-Logica-Programacao/Dia-4-JavaScript-Objetos-e-Funcoes/Exercicio-Pratico-Funcoes/Exercicio-Pratico6.js
@@ -0,0 +1,14 @@
+
+function somaNumeros(numeroFinal){
+  resultado = 0;
+  
+  for(var index = 0; index <= n; index += 1){
+    resultado = resultado + index;
+  }
+  
+  console.log("O resultado final e:",resultado)
+
+}
+
+n = 5;
+somaNumeros(n)
