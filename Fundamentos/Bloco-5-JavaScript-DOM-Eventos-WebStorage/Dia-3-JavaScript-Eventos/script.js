97c572c0053e0585db08a0cf8186451c8e3ac33c
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2021-09-08 19:23:59 -0300

    Exercicio 02 - Completo

--- Fundamentos/Bloco-5-JavaScript-DOM-Eventos-WebStorage/Dia-3-JavaScript-Eventos/script.js
+++ Fundamentos/Bloco-5-JavaScript-DOM-Eventos-WebStorage/Dia-3-JavaScript-Eventos/script.js
@@ -53,4 +53,14 @@ function insertDayInCalendar(arrayDias) {
 
 const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
-insertDayInCalendar(dezDaysList)
+insertDayInCalendar(dezDaysList);
+
+function createButtons(textButton, idButton) {
+  let button = document.createElement('button');
+  button.id = idButton
+  button.innerText = textButton
+  let dadElement = document.querySelector(".buttons-container")
+  dadElement.appendChild(button)
+}
+
+createButtons("Feriados", "btn-holiday")
