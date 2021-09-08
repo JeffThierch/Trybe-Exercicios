4756a2682ed35cc10017adb683995c8e8c5f0225
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2021-09-08 19:13:54 -0300

    Exercicio Pratico 01 - Completo

--- Fundamentos/Bloco-5-JavaScript-DOM-Eventos-WebStorage/Dia-3-JavaScript-Eventos/script.js
+++ Fundamentos/Bloco-5-JavaScript-DOM-Eventos-WebStorage/Dia-3-JavaScript-Eventos/script.js
@@ -13,4 +13,45 @@ function createDaysOfTheWeek() {
 
 createDaysOfTheWeek();
 
-// Escreva seu código abaixo.
+function addClassHoliday(element, day) {
+  if (day === 24 || day === 25 || day === 21) {
+    let holiday = element.classList = "day holiday";
+    return holiday;
+  }
+};
+
+function addClassFriday(element, day) {
+  if (day === 4 ||day === 11 ||day === 18 ||day === 25){
+    let friday = element.classList = "day friday";
+    return friday;
+  }
+};
+
+function insertDayInCalendar(arrayDias) {
+  /* Passo a Passo (Baby Steps) - 
+1- criar a funcao 
+2- criar o array dos dias
+3- criar o elemento li
+4- atribuir a classe ao elemento criado
+5- atribuir o elemento criado a seu pai
+6- iterar sobre os dias do array
+7- pegar dia a dia e adicionar ao elemento a seu respectivo dia da li 
+8- verificar se e feriado se sim adicionar a classe
+9- verificar se e sexta feira adicionar a classe*/
+
+  for(index = 0; index < dezDaysList.length; index += 1){
+    let listOfDays = document.createElement('li');
+    listOfDays.classList = "day"
+    let days = document.querySelector("#days")
+    days.appendChild(listOfDays)
+    listOfDays.innerText = dezDaysList[index]
+  
+    addClassHoliday(listOfDays, dezDaysList[index])
+    addClassFriday(listOfDays, dezDaysList[index])
+    
+  }
+}
+
+const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+insertDayInCalendar(dezDaysList)
