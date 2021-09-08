dc2a4d497b64cd1b43de717c2fc520f94844a783
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2021-09-08 16:01:30 -0300

    Requisito 03

--- Fundamentos/Bloco-5-JavaScript-DOM-Eventos-WebStorage/Dia-3-JavaScript-Eventos/Exercicios-Fixacao/script.js
+++ Fundamentos/Bloco-5-JavaScript-DOM-Eventos-WebStorage/Dia-3-JavaScript-Eventos/Exercicios-Fixacao/script.js
@@ -23,6 +23,10 @@ function changeText() {
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portifólio?
+thirdLi.addEventListener("dblclick", openNewPage)
+function openNewPage() {
+  window.open("https://jeffthierch.github.io/", "_blank")
+}
 
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
