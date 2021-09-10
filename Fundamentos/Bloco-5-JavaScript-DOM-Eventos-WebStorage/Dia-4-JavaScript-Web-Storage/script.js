2100b11e958bda11ecd6587994bcd4b808404861
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2021-09-10 11:11:04 -0300

    Altura da linha

--- Fundamentos/Bloco-5-JavaScript-DOM-Eventos-WebStorage/Dia-4-JavaScript-Web-Storage/script.js
+++ Fundamentos/Bloco-5-JavaScript-DOM-Eventos-WebStorage/Dia-4-JavaScript-Web-Storage/script.js
@@ -1,11 +1,16 @@
 window.onload  = function(){
   let fontSize = document.querySelector("#font-size-selector")
   let lineHeight = document.querySelector("#line-height-selector")
+  let backgroundColor = document.querySelector("#background-selector")
   let content = document.querySelector("#content")
   /* Define o tamanho da fonte do conteudo */
   fontSize.addEventListener("change", function(event){
     let tamanhoFonte = event.target.value
     content.style.fontSize = tamanhoFonte.toString() + "px"
   })
-
+  /* Define a altura da linha */
+  lineHeight.addEventListener("change", function(event){
+    let alturaLinha = event.target.value
+    content.style.lineHeight = alturaLinha.toString() + "px"
+  })
 }
