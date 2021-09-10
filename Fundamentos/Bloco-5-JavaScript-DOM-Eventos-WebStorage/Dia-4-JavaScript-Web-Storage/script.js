e8d9954fda97a10c55f115ebd1e3762b6556b687
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2021-09-10 11:27:37 -0300

    Font Color

--- Fundamentos/Bloco-5-JavaScript-DOM-Eventos-WebStorage/Dia-4-JavaScript-Web-Storage/script.js
+++ Fundamentos/Bloco-5-JavaScript-DOM-Eventos-WebStorage/Dia-4-JavaScript-Web-Storage/script.js
@@ -2,6 +2,7 @@ window.onload  = function(){
   let fontSize = document.querySelector("#font-size-selector")
   let lineHeight = document.querySelector("#line-height-selector")
   let backgroundColor = document.querySelector("#background-selector")
+  let fontColor = document.querySelector("#color-selector")
   let content = document.querySelector("#content")
   /* Define o tamanho da fonte do conteudo */
   fontSize.addEventListener("change", function(event){
@@ -17,5 +18,10 @@ window.onload  = function(){
   backgroundColor.addEventListener("change", function(event){
     let bgColor = event.target.value;
     document.body.style.backgroundColor = bgColor
+  })
+  /* Define cor da fonte */
+  fontColor.addEventListener("change", function(event){
+    let colorFont = event.target.value;
+    content.style.color = colorFont
   })
 }
