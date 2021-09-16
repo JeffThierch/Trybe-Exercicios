b21dcb0d78b211fb92a612f94b42e43755ca87a0
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2021-09-16 20:06:06 -0300

    Exercicio 6.1

--- Fundamentos/Bloco-6-HTML-CSS-Flexbox-e-Responsividade/Dia-1-HTML-CSS-Forms/script.js
+++ Fundamentos/Bloco-6-HTML-CSS-Flexbox-e-Responsividade/Dia-1-HTML-CSS-Forms/script.js
@@ -60,8 +60,6 @@ const initials = [
 'TO',
 ]
 
-
-
 function populateStatesOptions(fatherElement ,arrayStates, initialsArray ) {
   for (var i = 0; i < arrayStates.length; i += 1) {
     const option = document.createElement('option')
@@ -73,3 +71,158 @@ function populateStatesOptions(fatherElement ,arrayStates, initialsArray ) {
 }
 
 populateStatesOptions(stateSelect, statesOfBrazil, initials);
+
+const submitBtn = document.getElementById('btn-submit')
+
+
+
+const nameInput = document.getElementById('name-input')
+const emailInput = document.getElementById('email-input')
+const cpfInput = document.getElementById('cpf-input')
+const addressInput = document.getElementById('address-input')
+const cityInput = document.getElementById('city-input')
+const selectInput = document.getElementById('state-select')
+const radioSelect1 = document.getElementById('type-residence-input1')
+const radioSelect2 = document.getElementById('type-residence-input2')
+const resumeInput = document.getElementById('resumeInput')
+const jobInput = document.getElementById('job-input')
+const jobResume = document.getElementById('job-resume')
+const days = document.getElementById('days')
+const months = document.getElementById('months')
+const years = document.getElementById('years')
+
+function isNameInputValid() {
+  let control = true;
+  if (nameInput.value.trim() === "") {
+    control = false
+    return control;
+  } else {
+    return control;
+  }
+}
+
+function isEmailInputValid() {
+  let control = true
+  if (emailInput.value.split('').includes('@')) {
+    control = true
+    return control
+  } else {
+   control = false
+   return control
+  }
+}
+
+function limitCpfLength() {
+  if (cpfInput.value.length <= 11) {
+    cpfInput.value = cpfInput.value.slice(0, 10)
+  }
+}
+cpfInput.addEventListener('keydown', limitCpfLength)
+
+function isAdressInputValid() {
+  let control = true
+  if (addressInput.value.trim() === "") {
+    control = false
+    return control
+  } else {
+    control = true
+    return control;
+  }
+}
+
+function isResumeInputValid() {
+  let control = true
+  if (resumeInput.value.trim() === "") {
+    control = false
+    return control
+  } else {
+    control = true
+    return control;
+  }
+}
+
+function isJobInputValid() {
+  let control = true
+  if (jobInput.value.trim() === "") {
+    control = false
+    return control
+  } else {
+    control = true
+    return control;
+  }
+}
+
+function isJobResumeInputValid() {
+  let control = true
+  if (jobResume.value.trim() === "") {
+    control = false
+    return control
+  } else {
+    control = true
+    return control;
+  }
+}
+
+function generateDayAndMonth() {
+  /* generate days */
+  for (let i = 1; i <= 31; i += 1) {
+    let dayOption = document.createElement('option')
+    dayOption.value = i
+    dayOption.innerText = i
+    days.appendChild(dayOption)
+  }
+  /* generate months */
+  for (let i = 1; i <= 12; i += 1) {
+    let monthOption = document.createElement('option')
+    monthOption.value = i
+    monthOption.innerText = i
+    month.appendChild(monthOption)
+  }
+}
+
+generateDayAndMonth()
+
+function verifyIfYearIsValid() {
+  let yearValue = years.value
+  let yearToNumber = parseInt(yearValue, 10)
+  if (yearToNumber < 0) {
+    yearValue = "0"
+  }
+}
+
+years.addEventListener('change', verifyIfYearIsValid)
+
+const checkedValue = document.querySelector('input[name="residence"]:checked') 
+
+function createForm() {
+  const divContainer = document.createElement('div')
+  const createParagraph = document.createElement('p')
+  document.body.appendChild(divContainer)
+  const values = [nameInput.value, emailInput.value, cpfInput.value, addressInput.value, cityInput.value, selectInput.value, checkedValue.value,resumeInput.value, jobInput.value, jobResume.value]
+  const idValues = ['Nome: ', 'Email: ', 'Cpf: ', 'Address: ', 'City: ', 'State: ', 'Type of Residence: ','Resume: ', 'Job: ', 'Job Description: ']
+  for (let i = 0; i < values.length; i += 1) {
+    const createParagraph = document.createElement('p')
+    createParagraph.innerText = `${idValues[i]}${values[i]}`
+    divContainer.appendChild(createParagraph)
+  }
+  createParagraph.innerText = `Date of start: ${days.value}/${month.value}/${years.value}`
+  divContainer.appendChild(createParagraph)
+}
+
+
+
+function btnPreventDefault(event) {
+  event.preventDefault()
+  let nameValid = isNameInputValid()
+  let emailValid = isEmailInputValid()
+  let adressValid = isAdressInputValid()
+  let resumeValid = isResumeInputValid()
+  let jobValid = isJobInputValid()
+  let jobResumeValid = isJobResumeInputValid()
+  if (nameValid === true && emailValid === true && adressValid === true && resumeValid === true
+    && jobValid === true && jobResumeValid === true) {
+      createForm()
+    }
+}
+
+submitBtn.addEventListener('click', btnPreventDefault);
