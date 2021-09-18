469a478bb304b63b846b8da6b54443d66fdc4410
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2021-09-18 17:10:24 -0300

    Exercicio 6.2 finalizado

--- Fundamentos/Bloco-6-HTML-CSS-Flexbox-e-Responsividade/Dia-2-Bibliotecas-JavaScript-e-FrameWorks-CSS/script.js
+++ Fundamentos/Bloco-6-HTML-CSS-Flexbox-e-Responsividade/Dia-2-Bibliotecas-JavaScript-e-FrameWorks-CSS/script.js
@@ -1,238 +1,152 @@
-const stateSelect = document.getElementById('state-select');
+import './node_modules/just-validate/dist/js/just-validate.js'
 
-const statesOfBrazil = [
-'AC - Acre','AL - Alagoas','AP - Amapá','AM - Amazonas','BA - Bahia','CE - Ceará','DF - Distrito Federal','ES - Espírito Santo','GO - Goías','MA - Maranhão','MT - Mato Grosso','MS - Mato Grosso do Sul','MG - Minas Gerais','PA - Pará','PB - Paraíba','PR - Paraná','PE - Pernambuco','PI - Piauí','RJ - Rio de Janeiro','RN - Rio Grande do Norte','RS - Rio Grande do Sul','RO - Rondônia','RR - Roraíma','SC - Santa Catarina','SP - São Paulo','SE - Sergipe','TO - Tocantins'
-]
-
-const initials = [
-'AC','AL','AP','AM','BA','CE','DF','ES','GO','MA','MS','MT','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO',
-]
-
-function populateStatesOptions(fatherElement ,arrayStates, initialsArray ) {
-  for (var i = 0; i < arrayStates.length; i += 1) {
+function populateStatesOptions() {
+  const stateSelect = document.getElementById('state-select');
+  const statesOfBrazil = [
+  'AC - Acre','AL - Alagoas','AP - Amapá','AM - Amazonas','BA - Bahia','CE - Ceará','DF - Distrito Federal','ES - Espírito Santo','GO - Goías','MA - Maranhão','MT - Mato Grosso','MS - Mato Grosso do Sul','MG - Minas Gerais','PA - Pará','PB - Paraíba','PR - Paraná','PE - Pernambuco','PI - Piauí','RJ - Rio de Janeiro','RN - Rio Grande do Norte','RS - Rio Grande do Sul','RO - Rondônia','RR - Roraíma','SC - Santa Catarina','SP - São Paulo','SE - Sergipe','TO - Tocantins'
+  ]
+  const initials = [
+  'AC','AL','AP','AM','BA','CE','DF','ES','GO','MA','MS','MT','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO',
+  ]
+  for (var i = 0; i < statesOfBrazil.length; i += 1) {
     const option = document.createElement('option')
-    option.value = initialsArray[i]
-    option.innerText = arrayStates[i]
-    fatherElement.appendChild(option)
+    option.value = initials[i]
+    option.innerText = statesOfBrazil[i]
+    stateSelect.appendChild(option)
       
   }
 }
 
-populateStatesOptions(stateSelect, statesOfBrazil, initials);
+const btnClear = document.getElementById('btn-clear')
 
-const submitBtn = document.getElementById('btn-submit')
-const nameInput = document.getElementById('name-input')
-const emailInput = document.getElementById('email-input')
-const cpfInput = document.getElementById('cpf-input')
-const addressInput = document.getElementById('address-input')
-const cityInput = document.getElementById('city-input')
-const selectInput = document.getElementById('state-select')
-const radioSelect1 = document.getElementById('type-residence-input1')
-const radioSelect2 = document.getElementById('type-residence-input2')
-const resumeInput = document.getElementById('resumeInput')
-const jobInput = document.getElementById('job-input')
-const jobResume = document.getElementById('job-resume')
-const startDate = document.getElementById('startDate')
-const sendDataDiv = document.getElementById('dados-enviados')
-const errorDiv = document.getElementById('mensagemErro')
-const error = document.getElementById('error')
-let errorMsg = ''
+function clearAll() {
+  const inputs = document.getElementsByTagName('input')
+  const textAreas = document.getElementsByTagName('textarea')
 
-function addErrorToDiv(message) {
-  const errorParagraph = document.createElement('p');
-  errorParagraph.className = 'error'
-  errorParagraph.innerText = message;
-  errorDiv.appendChild(errorParagraph);
+  for (let i = 0; i < inputs.length; i += 1 ) {
+    inputs[i].value = ''
+  }
+  for (let i = 0; i < textAreas.length; i += 1) {
+    textAreas[i].value = ''
+  }
+}
+
+btnClear.addEventListener('click', clearAll)
+
+const picker = new Pikaday({field: document.getElementById('datepicker'),
+format: 'D MMM YYYY',
+toString(date, format) {
+  const day = date.getDate();
+  const month = date.getMonth() + 1;
+  const year = date.getFullYear();
+  return `${day}/${month}/${year}`; 
+},
+parse(dateString, format) {
+  // dateString is the result of `toString` method
+  const parts = dateString.split('/');
+  const day = parseInt(parts[0], 10);
+  const month = parseInt(parts[1], 10) - 1;
+  const year = parseInt(parts[2], 10);
+  return new Date(year, month, day);
+  }
+});
+
+
+window.onload = function () {
+  populateStatesOptions()
+  
+  new JustValidate('.js-form', {
+    rules: {
+      name: {
+        required: true,
+        minLength: 3,
+        maxLength: 40
+      },
+      email: {
+        required: true,
+        email: true,
+        maxLength: 50
+      },
+      cpf: {
+        required: true,
+        maxLength: 11
+      },
+      address: {
+        required: true,
+        maxLength: 200
+      },
+      city: {
+        required: true,
+        maxLength: 28
+      },
+      state: {
+        required: true,
+      },
+      radio: {
+        required: true
+      },
+      resume: {
+        required: true,
+        maxLength: 1000
+      },
+      jobInput: {
+        required: true,
+        maxLength: 40
+      },
+      jobResume: {
+        required: true,
+        maxLength: 500
+      },
+      date: {
+        required: true
+      }
+    },
+    messages: {
+      name: {
+        required: 'O campo de nome é obrigatório.',
+        maxLength: 'O limite é de 40 caracteres.'
+      },
+      email: {
+        required: 'O campo de email é obrigatório.',
+        email: 'O email digitado não é válido.',
+        maxLength: 'O limite é de 50 caracteres.'
+      },
+      cpf: {
+        required: 'O campo de CPF é obrigatório.',
+        maxLength: 'O limite é de 11 caracteres.'
+      },
+      address: {
+        required: 'O campo endereço é obrigatório.',
+        maxLength: 'O limite é de 200 caracteres.'
+      },
+      city: {
+        required: 'O campo cidade é obrigatório.',
+        maxLength: 'O limite é de 28 caracteres.'
+      },
+      state: {
+        required: 'O campo estado é obrigatório.',
+      },
+      radio: {
+        required: 'A escolha de um item é obrigatória.',
+      },
+      resume: {
+        required: 'Este campo é obrigatório.',
+        maxLength: 'O limite é de 1000 caracteres.'
+      },
+      jobInput: {
+        required: 'Este campo é obrigatório.',
+        maxLength: 'O limite é de 40 caracteres.'
+      },
+      jobResume: {
+        required: 'Este campo é obrigatório.',
+        maxLength: 'O limite é de 500 caracteres.'
+      },
+      date: {
+        required: 'Este campo é obrigatório.',
+      }
+    },
+    submitHandler: function(form, values) {
+        console.log(form, values)
+    }});
   
 }
-
-function isNameInputValid() {
-  let control = true;
-  if (nameInput.value.trim() === "") {
-    addErrorToDiv('Nome Invalido')
-    control = false
-    return control;
-  } else {
-    return control;
-  }
-}
-
-function isEmailInputValid() {
-  let control = true
-  if (emailInput.value.split('').includes('@')) {
-    control = true
-    return control
-  } else {
-    addErrorToDiv('Email Invalido')
-    control = false
-    return control
-  }
-}
-
-function limitCpfLength() {
-  if (cpfInput.value.length <= 11) {
-    cpfInput.value = cpfInput.value.slice(0, 10)
-  }
-}
-cpfInput.addEventListener('keydown', limitCpfLength)
-
-function isCpfValid() {
-  if (cpfInput.value.trim() === '') {
-    addErrorToDiv('CPF invalido')
-    control = false
-    return control
-  } else {
-    control = true
-    return control
-  }
-}
-
-function isAdressInputValid() {
-  let control = true
-  if (addressInput.value.trim() === "") {
-    addErrorToDiv('Endereco Invalido')
-    control = false
-    return control
-  } else {
-    control = true
-    return control;
-  }
-}
-
-function isResumeInputValid() {
-  let control = true
-  if (resumeInput.value.trim() === "") {
-    addErrorToDiv('Resumo Invalido')
-    control = false
-    return control
-  } else {
-    control = true
-    return control;
-  }
-}
-
-function isJobInputValid() {
-  let control = true
-  if (jobInput.value.trim() === "") {
-    addErrorToDiv('Insira um Trabalho')
-    control = false
-    return control
-  } else {
-    control = true
-    return control;
-  }
-}
-
-function isJobResumeInputValid() {
-  let control = true
-  if (jobResume.value.trim() === "") {
-    addErrorToDiv('Resumo do trabalho Invalido')
-    control = false
-    return control
-  } else {
-    control = true
-    return control;
-  }
-}
-
-const checkedValue = document.querySelector('[name=residence]:checked') 
-
-function dateValidation(){
-  let control = true
-  if(startDate.value.length === 0){
-    control = false
-    addErrorToDiv('A data não foi preenchida!')
-    return control
-  }
-
-  let regex = /^\d\d\/\d\d\/\d\d\d\d$/;
-  
-  if(!regex.test(startDate.value)){
-    control = false
-    addErrorToDiv('Data: Formato inválido')
-    return control
-  }
-
-  let splitted = startDate.value.split('/');
-  let day = splitted[0];
-  let month = splitted[1];
-  let year = splitted[2];
-
-  if(day < 0 || day > 30){
-    control = false
-    addErrorToDiv('Dia inválido')
-    return control
-  }
-
-  if(month < 0 || month > 12){
-    control = false
-    addErrorToDiv('Mês inválido')
-    return control
-  }
-
-  if(year < 0) {
-    control = false
-    addErrorToDiv('Ano inválido')
-    return control
-    
-  }
-  control = true
-  return control;
-}
-
-function createForm() {
-  const createParagraph = document.createElement('p')
-  document.body.appendChild(sendDataDiv)
-  const values = [nameInput.value, emailInput.value, cpfInput.value, addressInput.value, cityInput.value, selectInput.value, checkedValue.value,resumeInput.value, jobInput.value, jobResume.value]
-  const idValues = ['Nome: ', 'Email: ', 'Cpf: ', 'Address: ', 'City: ', 'State: ', 'Type of Residence: ','Resume: ', 'Job: ', 'Job Description: ']
-  for (let i = 0; i < values.length; i += 1) {
-    const createParagraph = document.createElement('p')
-    createParagraph.innerText = `${idValues[i]}${values[i]}`
-    sendDataDiv.appendChild(createParagraph)
-  }
-  createParagraph.innerText = `Data de inicio: ${startDate.value}`
-  sendDataDiv.appendChild(createParagraph)
-}
-
-function deleteErrorDiv() {
-  while (errorDiv.firstChild) {
-    errorDiv.removeChild(errorDiv.lastChild)
-  }
-}
-
-function clearDataDiv() {
-  while (sendDataDiv.firstChild) {
-    sendDataDiv.removeChild(sendDataDiv.lastChild)
-  }
-}
-
-function btnPreventDefault(event) {
-  event.preventDefault()
-  deleteErrorDiv()
-  let nameValid = isNameInputValid()
-  let emailValid = isEmailInputValid()
-  let adressValid = isAdressInputValid()
-  let resumeValid = isResumeInputValid()
-  let jobValid = isJobInputValid()
-  let jobResumeValid = isJobResumeInputValid()
-  let cpfValid = isCpfValid()
-  let startDateValid = dateValidation()
-  if(nameValid === true && emailValid === true && adressValid === true && resumeValid === true
-    && jobValid === true && jobResumeValid === true && cpfValid === true && startDateValid === true && errorDiv.children.length > 0) {
-      deleteErrorDiv()
-      createForm()
-    }
-  else if (nameValid === true && emailValid === true && adressValid === true && resumeValid === true
-    && jobValid === true && jobResumeValid === true && cpfValid === true && startDateValid === true && sendDataDiv.children.length > 0) {
-    clearDataDiv()
-    createForm()
-  }
-  else if (nameValid === true && emailValid === true && adressValid === true && resumeValid === true
-    && jobValid === true && jobResumeValid === true && cpfValid === true && startDateValid === true) {
-      createForm()
-    } else {
-      clearDataDiv()
-    }
-}
-
-submitBtn.addEventListener('click', btnPreventDefault);
