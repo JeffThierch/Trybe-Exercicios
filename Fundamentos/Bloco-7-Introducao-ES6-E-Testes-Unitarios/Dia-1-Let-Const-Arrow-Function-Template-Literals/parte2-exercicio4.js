d92fe53efa3d7da7addfc4887c4d135111fbaa15
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2021-09-25 14:02:12 -0300

    Parte 2 - exercicio 4

---
+++ Fundamentos/Bloco-7-Introducao-ES6-E-Testes-Unitarios/Dia-1-Let-Const-Arrow-Function-Template-Literals/parte2-exercicio4.js
@@ -0,0 +1,16 @@
+/* Exercicio 4 */
+
+const replaceX = (frase, stingTrocada) => {
+  let novaSting;
+  if (frase.includes('x') === true) {
+    novaSting = frase.replace('x', stingTrocada)
+  } else {
+    return console.log('A frase nao possui (x)');
+  }
+  console.log(novaSting)
+}
+const arraySkills = ['CSS' , 'HTML', 'JS']
+
+replaceX("Tryber x aqui!", Garai)
+
+/* Exercio 5 */
--- Fundamentos/Bloco-7-Introducao-ES6-E-Testes-Unitarios/Dia-1-Let-Const-Arrow-Function-Template-Literals/parte2.js
+++ Fundamentos/Bloco-7-Introducao-ES6-E-Testes-Unitarios/Dia-1-Let-Const-Arrow-Function-Template-Literals/parte2.js
@@ -10,34 +10,3 @@ const fatorial = (numero) => {
 }
 
 console.log(fatorial(5))
-
-
-
-/* Exercio 2 */
-
-const longestWord = (frase) => {
-  let controle = 0;
-  let palavra;
-  const fraseSplit = frase.split(' ')
-  for (let i = 0; i < fraseSplit.length; i += 1 ) {
-    if (fraseSplit[i].length > controle) {
-      controle = fraseSplit[i].length;
-      palavra = fraseSplit[i];
-    }
-  }
-  console.log(palavra)
-}
-
-longestWord("Antônio foi no banheiro e não sabemos o que aconteceu")
-
-/* Exercicio 3 */
-
-const btnclick = document.getElementById('btn-click')
-let clickCount = 0
-
-const clickCounter = () => {
-  clickCount += 1
-  const counter = document.getElementById('contador').innerHTML = clickCount
-}
-
-btnclick.addEventListener('click', clickCounter)
