65272dd6c7023d08375af2249b926fcea2131b1b
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2021-09-27 19:38:02 -0300

    Requisitos Bonus

--- Fundamentos/Bloco-7-Introducao-ES6-E-Testes-Unitarios/Dia-2-Objects/exercicioPT2.js
+++ Fundamentos/Bloco-7-Introducao-ES6-E-Testes-Unitarios/Dia-2-Objects/exercicioPT2.js
@@ -56,7 +56,58 @@ const totalOfStudents = (object) => {
 
 console.log(totalOfStudents(allLessons))
 
-/* Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto */
+/* 7- Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto */
 
 const valueInPosition = (object, index) => Object.values(object)[index];
 console.log(valueInPosition(lesson1, 0));
+
+/* 8- Crie uma função que verifique se o par (chave / valor) existe na função. Essa função deve possuir três parâmetros, sendo eles: o objeto, o nome da chave e o valor da chave.  */
+
+const verifyKeyValue = (object, key, value) => {
+  const entries = Object.entries(object)
+  let equal = false
+  for (const i in entries) {
+    if ((entries[i][0] === key && entries[i][1] === value)) {
+       equal = true
+     }
+  }
+  return equal
+}
+console.log(verifyKeyValue(lesson3, 'turno', 'dia'))
+
+/* BONUS 01 - Crie uma função para contar quantos estudantes assistiram às aulas de Matemática. Use o objeto criado no exercício 5. */
+
+const countStudentsOfMath = (object) => {
+ let total = 0;
+ const arrayAulas = Object.keys(object);
+ for (i in arrayAulas) {
+  if (object[arrayAulas[i]].materia === "Matemática") {
+    total += object[arrayAulas[i]].numeroEstudantes
+  }
+ }
+ return console.log(total)
+}
+countStudentsOfMath(allLessons)
+
+
+const info = (name) => {
+  let lessons = [];
+  let students = 0;
+  const valuesArray = Object.values(allLessons);
+  for (i in valuesArray) {
+    if (valuesArray[i].professor === name) {
+      lessons.push(valuesArray[i].materia)
+      students += valuesArray[i].numeroEstudantes
+    }
+  }
+  return {tarefas : lessons, estudantes: students }
+}
+
+const createReport = (allLessons, name) => {
+  const report = {}
+  report.professor = name;
+  Object.assign(report, info(name));
+  return report
+}
+
+console.log(createReport(allLessons, 'Maria Clara'));
