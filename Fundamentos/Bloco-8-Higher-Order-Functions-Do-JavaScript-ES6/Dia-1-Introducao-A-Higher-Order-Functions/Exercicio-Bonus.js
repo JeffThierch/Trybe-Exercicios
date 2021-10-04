9698178cd3219be00afd7f0839512b05fcce997b
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2021-10-04 20:21:10 -0300

    Exercicio Bonus - pt2.1

--- Fundamentos/Bloco-8-Higher-Order-Functions-Do-JavaScript-ES6/Dia-1-Introducao-A-Higher-Order-Functions/Exercicio-Bonus.js
+++ Fundamentos/Bloco-8-Higher-Order-Functions-Do-JavaScript-ES6/Dia-1-Introducao-A-Higher-Order-Functions/Exercicio-Bonus.js
@@ -18,16 +18,48 @@ const dragon = {
   damage: undefined,
 };
 
-const generateDragonDamage = () => Math.floor(Math.random() * (dragon.strength - 15)) + 15;
-const generateWarriorDamage = () => Math.floor(Math.random() * (warrior.strength - warrior.weaponDmg)) + warrior.weaponDmg;
-const generateMageDamage = () => {
-  const mageDamage = Math.floor(Math.random() * ((mage.intelligence * 2) - mage.intelligence)) + mage.intelligence
-  const mageManaConsumption = 15
+const dragonAttack = () => {
+  const minAttack = 15
+  const maxAttack = dragon.strength
+  return Math.floor(Math.random() * (maxAttack - minAttack + 1)) + minAttack;
+}
+
+const warriorAttack = () => {
+  const minAttack = warrior.weaponDmg;
+  const maxAttack = warrior.strength;
+  return Math.floor(Math.random() * (maxAttack - minAttack + 1)) + minAttack;
+}
+
+const mageAttack = () => {
+  const minAttack = mage.intelligence;
+  const maxAttack = mage.intelligence * 2;
+  const mageDamage = Math.floor(Math.random() * (maxAttack - minAttack + 1)) + minAttack;
+  const mageManaConsumption = 15;
+
   if (mage.mana < mageManaConsumption) {
     return 'Não possui mana suficiente'
   }
-  return {dano: mageDamage, manaUtilizada: mageManaConsumption };
+
+  return {damage: mageDamage, manaSpent: mageManaConsumption };
 }
-console.log(generateMageDamage())
 
 const battleMembers = { mage, warrior, dragon };
+
+
+const gameActions = {
+  warriorTurn: (warriorAttack) => {
+    const damage = warriorAttack()
+    warrior.damage = damage
+    dragon.healthPoints -= damage;
+  },
+  mageTurn: (mageAttack) => {
+    const attackStats = mageAttack()
+    mage.damage = attackStats.damage
+    mage.mana -= attackStats.manaSpent
+    dragon.healthPoints -= mage.damage
+  }
+};
+
+gameActions.warriorTurn(warriorAttack)
+gameActions.mageTurn(mageAttack)
+
