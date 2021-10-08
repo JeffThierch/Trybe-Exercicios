a0202c6753721d13d6e1143e5261d4e61e8889ba
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2021-10-08 15:35:29 -0300

    Exercicio 06 - Completo

--- Fundamentos/Bloco-8-Higher-Order-Functions-Do-JavaScript-ES6/Dia-5-Higher-Order-Functions-Spread-Operator-Parametro-Rest-Destructuring-e-Mais/exercicio06.js
+++ Fundamentos/Bloco-8-Higher-Order-Functions-Do-JavaScript-ES6/Dia-5-Higher-Order-Functions-Spread-Operator-Parametro-Rest-Destructuring-e-Mais/exercicio06.js
@@ -3,3 +3,13 @@ const shelbyCobra = ['Shelby Cobra', 'Ford', 1963];
 const chiron = ['Chiron', 'Bugatti', 2016];
 
 // escreva toObject abaixo
+
+const toObject = ([nome, marca, ano]) => {
+  return {
+    nome,
+    marca,
+    ano
+  }
+}
+
+console.log(toObject(palio));
