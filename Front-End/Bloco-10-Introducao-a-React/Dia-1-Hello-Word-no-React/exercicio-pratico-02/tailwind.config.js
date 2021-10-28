99f2eb98071c6fda01d49396bedd2ca7af35be53
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2021-10-28 10:20:27 -0300

    Exercicio 03 Responsivo

--- Front-End/Bloco-10-Introducao-a-React/Dia-1-Hello-Word-no-React/exercicio-pratico-02/tailwind.config.js
+++ Front-End/Bloco-10-Introducao-a-React/Dia-1-Hello-Word-no-React/exercicio-pratico-02/tailwind.config.js
@@ -3,7 +3,12 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
    darkMode: false, // or 'media' or 'class'
    theme: {
-     extend: {},
+     extend: {
+      height: {
+        maxContent: 'max-content',
+        customHeight: '81vh'
+       }
+     },
    },
    variants: {
      extend: {},
