a94428f0e6d69fcb81781ec21612d2da8d11cd07
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2022-01-08 16:40:31 -0300

    Exercicio-02.4

--- Back-End/Bloco-20-Introducao-a-SQL/Dia-2-Encontrando-Dados-Em-Um-Banco-De-Dados/Exercicio-02/Exercicio02.sql
+++ Back-End/Bloco-20-Introducao-a-SQL/Dia-2-Encontrando-Dados-Em-Um-Banco-De-Dados/Exercicio-02/Exercicio02.sql
@@ -6,3 +6,7 @@ WHERE Provider = 'RBT';
 SELECT * FROM Provides ORDER BY Price DESC LIMIT 5;
 
 SELECT Provider, Price FROM Provides ORDER BY Price DESC LIMIT 4 OFFSET 3;
+
+SELECT Code, Name, Provider, Price FROM Pieces, Provides
+WHERE Code = Piece AND Provider = 'HAL'
+ORDER BY Price DESC;
