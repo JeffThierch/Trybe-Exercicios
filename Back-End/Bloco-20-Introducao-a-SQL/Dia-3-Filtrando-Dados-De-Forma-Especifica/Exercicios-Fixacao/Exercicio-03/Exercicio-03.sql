9046249e84a0926ba5927e851af7d502e020a323
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2022-01-12 17:51:01 -0300

    Exercicio03.5

--- Back-End/Bloco-20-Introducao-a-SQL/Dia-3-Filtrando-Dados-De-Forma-Especifica/Exercicios-Fixacao/Exercicio-03/Exercicio-03.sql
+++ Back-End/Bloco-20-Introducao-a-SQL/Dia-3-Filtrando-Dados-De-Forma-Especifica/Exercicios-Fixacao/Exercicio-03/Exercicio-03.sql
@@ -14,3 +14,8 @@ WHERE payment_date BETWEEN '2005-05-01' AND '2005-08-01';
 SELECT title, release_year, rental_duration FROM film
 WHERE rental_duration BETWEEN 3 AND 6
 ORDER BY rental_duration DESC, title;
+
+SELECT title, rating FROM film
+WHERE rating IN ('G', 'PG', 'PG-13')
+ORDER BY title
+LIMIT 500;
