376e84d44317f90c62c61c713927aaacdafe9d4b
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2022-01-12 18:19:24 -0300

    Exercicio04.4

--- Back-End/Bloco-20-Introducao-a-SQL/Dia-3-Filtrando-Dados-De-Forma-Especifica/Exercicios-Fixacao/Exercicio-04/Exercicio-04.sql
+++ Back-End/Bloco-20-Introducao-a-SQL/Dia-3-Filtrando-Dados-De-Forma-Especifica/Exercicios-Fixacao/Exercicio-04/Exercicio-04.sql
@@ -7,5 +7,11 @@ BETWEEN '2005-07-01' AND '2005-08-22';
 SELECT COUNT(*) FROM payment
 WHERE DATE(payment_date) = '2005-05-25';
 
-SELECT rental_date from rental
+SELECT rental_date FROM rental
 WHERE rental_id = 10330;
+
+SELECT * FROM payment
+WHERE payment_date
+BETWEEN '2005-07-28 22:00:00' 
+AND '2005-07-28 23:59:59'
+ORDER BY payment_date;
