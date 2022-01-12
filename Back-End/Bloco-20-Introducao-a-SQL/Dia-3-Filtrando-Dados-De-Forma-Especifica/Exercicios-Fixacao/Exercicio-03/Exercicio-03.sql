eed001fbabd7741afb20af9d9af3467f83c683a3
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2022-01-12 17:46:18 -0300

    Exercicio03.3

--- Back-End/Bloco-20-Introducao-a-SQL/Dia-3-Filtrando-Dados-De-Forma-Especifica/Exercicios-Fixacao/Exercicio-03/Exercicio-03.sql
+++ Back-End/Bloco-20-Introducao-a-SQL/Dia-3-Filtrando-Dados-De-Forma-Especifica/Exercicios-Fixacao/Exercicio-03/Exercicio-03.sql
@@ -7,3 +7,6 @@ ORDER BY first_name;
 SELECT email FROM customer
 WHERE address_id BETWEEN 172 AND 176
 ORDER BY email;
+
+SELECT	COUNT( * ) FROM payment
+WHERE payment_date BETWEEN '2005-05-01' AND '2005-08-01';
