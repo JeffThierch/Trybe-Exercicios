7950320799b85443121034bdd506b503df01251f
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2022-01-12 17:48:48 -0300

    Exercicio03.4

--- Back-End/Bloco-20-Introducao-a-SQL/Dia-3-Filtrando-Dados-De-Forma-Especifica/Exercicios-Fixacao/Exercicio-03/Exercicio-03.sql
+++ Back-End/Bloco-20-Introducao-a-SQL/Dia-3-Filtrando-Dados-De-Forma-Especifica/Exercicios-Fixacao/Exercicio-03/Exercicio-03.sql
@@ -10,3 +10,7 @@ ORDER BY email;
 
 SELECT	COUNT( * ) FROM payment
 WHERE payment_date BETWEEN '2005-05-01' AND '2005-08-01';
+
+SELECT title, release_year, rental_duration FROM film
+WHERE rental_duration BETWEEN 3 AND 6
+ORDER BY rental_duration DESC, title;
