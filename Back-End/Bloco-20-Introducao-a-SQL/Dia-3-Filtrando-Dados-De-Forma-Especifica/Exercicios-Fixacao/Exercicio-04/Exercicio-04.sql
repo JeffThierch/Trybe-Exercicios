b519d530233a8c80527d6bc21fc33cb04dc07fdb
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2022-01-12 18:14:10 -0300

    Exercicio04.2

--- Back-End/Bloco-20-Introducao-a-SQL/Dia-3-Filtrando-Dados-De-Forma-Especifica/Exercicios-Fixacao/Exercicio-04/Exercicio-04.sql
+++ Back-End/Bloco-20-Introducao-a-SQL/Dia-3-Filtrando-Dados-De-Forma-Especifica/Exercicios-Fixacao/Exercicio-04/Exercicio-04.sql
@@ -1,4 +1,8 @@
 USE sakila;
+
 SELECT COUNT(*) FROM payment
 WHERE DATE(payment_date) 
 BETWEEN '2005-07-01' AND '2005-08-22';
+
+SELECT COUNT(*) FROM payment
+WHERE DATE(payment_date) = '2005-05-25';
