836493d3ece0db0949a378c42d298cddb2ecd5be
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2022-01-12 16:06:09 -0300

    Exercicio01.3

--- Back-End/Bloco-20-Introducao-a-SQL/Dia-3-Filtrando-Dados-De-Forma-Especifica/Exercicios-Fixacao/Exercicio-01/Exercicio-01.sql
+++ Back-End/Bloco-20-Introducao-a-SQL/Dia-3-Filtrando-Dados-De-Forma-Especifica/Exercicios-Fixacao/Exercicio-01/Exercicio-01.sql
@@ -9,3 +9,13 @@ AND store_id = 2
 AND first_name <> 'KENNETH'
 ORDER BY first_name;
 
+SELECT title, description, release_year, replacement_cost, rating FROM film
+WHERE (rating = 'G' 
+OR rating = 'PG' 
+OR rating = 'PG-13')
+AND replacement_cost >= 18
+ORDER BY replacement_cost DESC, title ASC
+LIMIT 100;
+
+
+
