de29959b10872bb16e03d34ccb218d18550e03c5
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2022-01-12 17:42:52 -0300

    Exercicio03.2

--- Back-End/Bloco-20-Introducao-a-SQL/Dia-3-Filtrando-Dados-De-Forma-Especifica/Exercicios-Fixacao/Exercicio-03/Exercicio-03.sql
+++ Back-End/Bloco-20-Introducao-a-SQL/Dia-3-Filtrando-Dados-De-Forma-Especifica/Exercicios-Fixacao/Exercicio-03/Exercicio-03.sql
@@ -3,3 +3,7 @@ USE sakila;
 SELECT first_name, last_name, email FROM customer
 WHERE last_name IN ('hicks', 'crawford', 'henry', 'boyd', 'mason', 'morales', 'kennedy')
 ORDER BY first_name;
+
+SELECT email FROM customer
+WHERE address_id IN (172, 173, 174, 175, 176)
+ORDER BY email;
