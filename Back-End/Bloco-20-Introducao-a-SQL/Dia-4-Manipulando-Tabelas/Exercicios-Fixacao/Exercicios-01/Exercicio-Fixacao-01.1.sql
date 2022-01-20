130c27d28635ec314473e8a68522f014bc6fc322
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2022-01-20 15:45:44 -0300

    Exercicios-fixacao-01.1

---
+++ Back-End/Bloco-20-Introducao-a-SQL/Dia-4-Manipulando-Tabelas/Exercicios-Fixacao/Exercicios-01/Exercicio-Fixacao-01.1.sql
@@ -0,0 +1,8 @@
+USE sakila;
+
+SELECT * FROM staff;
+INSERT INTO staff (first_name, last_name, address_id, email, store_id, active, username)
+VALUES
+('JEFF', 'THIERCH', 3, 'jeff08_alexandre@homail.com', 2, 1, 'Jeff')
+;
+SELECT * FROM staff;
