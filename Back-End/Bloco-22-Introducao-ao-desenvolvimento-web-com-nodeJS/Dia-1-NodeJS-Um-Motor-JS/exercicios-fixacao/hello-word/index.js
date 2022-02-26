eb9a838fc89c600138b9a6a75f10ee621d9c7a49
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2022-02-26 15:40:46 -0300

    Feat: User Conversation with CLI"

--- Back-End/Bloco-22-Introducao-ao-desenvolvimento-web-com-nodeJS/Dia-1-NodeJS-Um-Motor-JS/exercicios-fixacao/hello-word/index.js
+++ Back-End/Bloco-22-Introducao-ao-desenvolvimento-web-com-nodeJS/Dia-1-NodeJS-Um-Motor-JS/exercicios-fixacao/hello-word/index.js
@@ -1,1 +1,7 @@
-console.log('Hello, word');
+const readlineSync = require('readline-sync');
+
+const name = readlineSync.question('Qual seu nome? ');
+const age = readlineSync.questionInt('Qual sua idade? ');
+
+console.log(`Hello, ${name}! You are ${age} years old!`);
+//console.log('Hello, word');
