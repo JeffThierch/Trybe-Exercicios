bb2fee98726b8152f3448acb4e20c36774e77344
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2022-03-01 16:54:36 -0300

    Feat: Exercicio-pratico-06-feat-trycatch

--- Back-End/Bloco-22-Introducao-ao-desenvolvimento-web-com-nodeJS/Dia-2-NodeJs-Fluxo-Assincrono/Exercicios-Praticos/Exercicio06.js
+++ Back-End/Bloco-22-Introducao-ao-desenvolvimento-web-com-nodeJS/Dia-2-NodeJs-Fluxo-Assincrono/Exercicios-Praticos/Exercicio06.js
@@ -1,9 +1,14 @@
 const fs = require('fs').promises;
 
 const removeIdsFromDocument = async () => {
-  const characterInfos = await fs.readFile('./simpsons.json', 'utf8');
-  const filteredCharacters = JSON.parse(characterInfos).filter(({id}) => id !== '10' && id !== '6')
-  await fs.writeFile('./simpsons.json', JSON.stringify(filteredCharacters, null, 2))
+  try {
+    const characterInfos = await fs.readFile('./simpsons.json', 'utf8');
+    const filteredCharacters = JSON.parse(characterInfos).filter(({id}) => id !== '10' && id !== '6')
+    await fs.writeFile('./simpsons.json', JSON.stringify(filteredCharacters, null, 2))
+  } catch (error) {
+    console.error(error.message);
+  }
+    
 }
 
 removeIdsFromDocument()
