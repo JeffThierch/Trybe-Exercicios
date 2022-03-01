853841910fef535081016fef05a7633352d0e1e9
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2022-03-01 17:57:28 -0300

    Feat: Exercicio-pratico-10

---
+++ Back-End/Bloco-22-Introducao-ao-desenvolvimento-web-com-nodeJS/Dia-2-NodeJs-Fluxo-Assincrono/Exercicios-Praticos/Exercicio10.js
@@ -0,0 +1,18 @@
+const fs = require('fs').promises;
+
+
+
+const createMultipleFiles = async () => {
+  const arrayOfWords = ['Finalmente', 'estou', 'usando', 'Promise.all', '!!!']
+  
+  await Promise.all(
+    arrayOfWords.map((item, index) => fs.writeFile(`./file${index + 1}.txt`, item))
+  )
+
+  const filesData = await Promise.all(
+    arrayOfWords.map((_, index) => fs.readFile(`./file${index + 1}.txt`, 'utf8'))
+  )
+  await fs.writeFile("./fileAll.txt", filesData.reduce((acc, curr) =>  `${acc} ${curr}`))
+}
+
+createMultipleFiles()
