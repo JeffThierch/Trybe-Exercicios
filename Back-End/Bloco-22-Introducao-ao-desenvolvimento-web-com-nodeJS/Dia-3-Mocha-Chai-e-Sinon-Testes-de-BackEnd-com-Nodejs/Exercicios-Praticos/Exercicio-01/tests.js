f82528dff850ab65eb77052b7425c9ac7ff1cf55
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2022-03-03 18:30:13 -0300

    feat: basic tdd for verifyNumberValue

---
+++ Back-End/Bloco-22-Introducao-ao-desenvolvimento-web-com-nodeJS/Dia-3-Mocha-Chai-e-Sinon-Testes-de-BackEnd-com-Nodejs/Exercicios-Praticos/Exercicio-01/tests.js
@@ -0,0 +1,33 @@
+const {expect} = request('chai');
+const sinon = require('sinon');
+
+const verifyNumberValue = require('./verifyNumberValue');
+
+describe('verifyNumberValue', () => {
+  describe('Quando o numero for', () => {
+    it('6 deve retornar "positivo" ', () => {
+
+      const returnedValue = verifyNumberValue(6);
+
+      expect(returnedValue).to.be.equals('positivo')
+      
+    });
+
+    it('-2 deve retornar "negativo" ', () => {
+      
+      const returnedValue = verifyNumberValue(-2);
+
+      expect(returnedValue).to.be.equals('negativo')
+
+    });
+
+    it('0 deve retornar "neutro" ', () => {
+
+      const returnedValue = verifyNumberValue(0);
+
+      expect(returnedValue).to.be.equals('neutro')
+
+    });
+
+  });
+});
