43885c8c0e1a30c58a11255c86e9269c2dcbe9be
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2022-03-05 14:28:32 -0300

    feat: Api-get-drinks-and-recipes-ordered

--- Back-End/Bloco-22-Introducao-ao-desenvolvimento-web-com-nodeJS/Dia-4-Express-HTTP-Com-NodeJS/Exercicios-Fixacao/api-get-drinks/index.js
+++ Back-End/Bloco-22-Introducao-ao-desenvolvimento-web-com-nodeJS/Dia-4-Express-HTTP-Com-NodeJS/Exercicios-Fixacao/api-get-drinks/index.js
@@ -18,6 +18,26 @@ const drinks = [
   { id: 6, name: 'Água Mineral 500 ml', price: 5.0 },
 ]
 
+const recipes = [
+  { id: 1, name: 'Lasanha', price: 40.0, waitTime: 30 },
+  { id: 2, name: 'Macarrão a Bolonhesa', price: 35.0, waitTime: 25 },
+  { id: 3, name: 'Macarrão com molho branco', price: 35.0, waitTime: 25 },
+]
+
+const sortByNameValue = (a, b) => {
+  if ( a.name < b.name ){
+    return -1;
+  }
+  if ( a.name > b.name ){
+    return 1;
+  }
+  return 0;
+}
+
+app.get('/recipes', (_req, res) => {
+  res.json(recipes.sort(sortByNameValue))
+})
+
 app.get('/drinks', (_req, res) => {
-  res.json(drinks)
+  res.json(drinks.sort(sortByNameValue))
 });
