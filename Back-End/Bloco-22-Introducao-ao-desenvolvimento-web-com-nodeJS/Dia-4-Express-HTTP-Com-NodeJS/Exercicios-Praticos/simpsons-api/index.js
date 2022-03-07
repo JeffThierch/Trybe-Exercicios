406e47810e632720fddecb4e6f5fd64696608579
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2022-03-07 20:11:22 -0300

    Feat: Exercicio-pratico-06

--- Back-End/Bloco-22-Introducao-ao-desenvolvimento-web-com-nodeJS/Dia-4-Express-HTTP-Com-NodeJS/Exercicios-Praticos/simpsons-api/index.js
+++ Back-End/Bloco-22-Introducao-ao-desenvolvimento-web-com-nodeJS/Dia-4-Express-HTTP-Com-NodeJS/Exercicios-Praticos/simpsons-api/index.js
@@ -10,13 +10,32 @@ app.listen(PORT, () => {
 
 app.use(bodyParser.json());
 
-app.get('/simpsons', (req, res) => {
+app.get('/simpsons', (_req, res) => {
   try {
     const simpsons = fs.readFileSync('./simpsons.json', 'utf8')
 
     return res.status(200).json(JSON.parse(simpsons))
   }
   catch (err) {
+    return res.status(500).json({message: 'Ocorreu um erro tente novamente!'})
+  }
+})
+
+app.get('/simpsons/:id', (req, res) => {
+  try {
+    const { id } = req.params;
+  
+    const simpsons = fs.readFileSync('./simpsons.json', 'utf8');
+  
+    const simpsonInfo = JSON.parse(simpsons).filter((simpson) => simpson.id === id)
+  
+    if(!simpsonInfo) {
+      return res.status(404).json({message: 'Simpson not found!'})
+    }
+  
+    return res.status(200).json(simpsonInfo)
+
+  } catch (err) {
     return res.status(500).json({message: 'Ocorreu um erro tente novamente!'})
   }
 })
