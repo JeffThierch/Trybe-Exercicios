b0c422a8789195e9a9b3770bf9420a5269379698
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2022-03-07 19:08:32 -0300

    Feat: Exercicio-pratico-01

--- Back-End/Bloco-22-Introducao-ao-desenvolvimento-web-com-nodeJS/Dia-4-Express-HTTP-Com-NodeJS/Exercicios-Praticos/api-pratica01/index.js
+++ Back-End/Bloco-22-Introducao-ao-desenvolvimento-web-com-nodeJS/Dia-4-Express-HTTP-Com-NodeJS/Exercicios-Praticos/api-pratica01/index.js
@@ -9,6 +9,13 @@ app.listen(PORT, () => {
 
 app.use(bodyParser.json());
 
-app.get('/ping', (req, res) => {
+app.get('/ping', (_req, res) => {
   res.status(200).json({message: 'pong'})
+});
+
+app.post('/hello', (req, res) => {
+  const { name } = req.body
+
+  res.status(201).json({message: `Hello, ${name}!`})
+
 })
