7c3d6c9469ceb88ced660bd3c60be62f36a59131
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2022-03-23 15:41:59 -0300

    Refactor: http codes and json response"

--- Back-End/Bloco-23-NodeJS-Camada-de-Servicos-e-Arquitetura-Rest-e-Restful/Dia-3-Arquitetura-Web-Rest-e-Restful/Exercicios-Praticos/refactoring/src/controllers/productController.js
+++ Back-End/Bloco-23-NodeJS-Camada-de-Servicos-e-Arquitetura-Rest-e-Restful/Dia-3-Arquitetura-Web-Rest-e-Restful/Exercicios-Praticos/refactoring/src/controllers/productController.js
@@ -3,13 +3,13 @@ const ProductModel = require('../models/productModel');
 const getAll = async (_req, res) => {
   const products = await ProductModel.getAll();
 
-  res.send(products);
+  res.status(200).json(products);
 };
 
 const getById = async (req, res, next) => {
   const product = await ProductModel.getById(req.params.id);
 
-  res.send(product);
+  res.status(200).json(product);;
 };
 
 const addProduct = async (req, res) => {
@@ -17,13 +17,13 @@ const addProduct = async (req, res) => {
 
   const newProduct = await ProductModel.add(name, brand);
 
-  res.send(newProduct);
+  res.status(201).json(newProduct);
 };
 
 const deleteProduct = async (req, res) => {
   const products = await ProductModel.exclude(req.params.id);
 
-  res.send(products);
+  res.status(200).json(products);
 };
 
 const editProduct = async (req, res) => {
@@ -31,7 +31,7 @@ const editProduct = async (req, res) => {
 
   const products = await ProductModel.update(req.params.id, name, brand);
 
-  res.send(products);
+  res.status(200).json(products);
 };
 
 module.exports = {
