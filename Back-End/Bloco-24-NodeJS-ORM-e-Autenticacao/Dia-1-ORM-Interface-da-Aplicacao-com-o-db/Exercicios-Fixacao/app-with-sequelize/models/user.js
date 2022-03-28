5be54dda792d963c17662a873433ff9eeef5fccc
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2022-03-28 12:22:46 -0300

    Feat: userModel convert phone_num to phoneNum

--- Back-End/Bloco-24-NodeJS-ORM-e-Autenticacao/Dia-1-ORM-Interface-da-Aplicacao-com-o-db/Exercicios-Fixacao/app-with-sequelize/models/user.js
+++ Back-End/Bloco-24-NodeJS-ORM-e-Autenticacao/Dia-1-ORM-Interface-da-Aplicacao-com-o-db/Exercicios-Fixacao/app-with-sequelize/models/user.js
@@ -2,7 +2,7 @@ const User = (sequelize, DataTypes) => {
   const User = sequelize.define("User", {
     fullName: DataTypes.STRING,
     email: DataTypes.STRING,
-    phone_num: DataTypes.STRING
+    phoneNum: DataTypes.STRING
   }, {
     underscore: true,
     tableName: 'Users'
