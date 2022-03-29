f2078191932d3c9f0b2fb9a2a942bd039f5bbe5a
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2022-03-29 17:14:32 -0300

    Feat: Sequelize Trasactions testsd

--- Back-End/Bloco-24-NodeJS-ORM-e-Autenticacao/Dia-2-ORM-Associations/Exercicios-Fixacao/exercicio-01/src/config/config.js
+++ Back-End/Bloco-24-NodeJS-ORM-e-Autenticacao/Dia-2-ORM-Associations/Exercicios-Fixacao/exercicio-01/src/config/config.js
@@ -10,9 +10,10 @@ module.exports = {
   test: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    database: process.env.DB_TEST,
     host: process.env.DB_HOST,
-    dialect: process.env.DB_USED
+    dialect: process.env.DB_USED,
+    logging: false
   },
   production: {
     username: process.env.DB_USER,
--- Back-End/Bloco-24-NodeJS-ORM-e-Autenticacao/Dia-2-ORM-Associations/Exercicios-Fixacao/exercicio-01/src/index.js
+++ Back-End/Bloco-24-NodeJS-ORM-e-Autenticacao/Dia-2-ORM-Associations/Exercicios-Fixacao/exercicio-01/src/index.js
@@ -1,8 +1,15 @@
 // index.js
 const express = require('express');
+const bodyParser = require('body-parser');
+const Sequelize = require('sequelize');
+
 const { Address, Employee } = require('./models');
+const config = require('./config/config');
 
 const app = express();
+app.use(bodyParser.json());
+
+const sequelize = new Sequelize(process.env.NODE_ENV === 'test' ? config.test : config.development);
 
 app.get('/employees', async (_req, res) => {
   try {
@@ -15,6 +22,28 @@ app.get('/employees', async (_req, res) => {
     console.log(e.message);
     res.status(500).json({ message: 'Ocorreu um erro' });
   };
+});
+
+app.post('/employees', async (req, res) => {
+  const t = await sequelize.transaction();
+
+  try {
+    const { firstName, lastName, age, city, street, number } = req.body;
+
+    const employee = await Employee.create({ firstName, lastName, age },  { transaction: t },);
+
+    await Address.create({ city, street, number, employeeId: employee.id },  { transaction: t },);
+
+    await t.commit();
+
+    return res.status(201).json({ 
+      id: employee.id,
+      message: 'Cadastrado com sucesso' });
+  } catch (e) {
+    await t.rollback();
+    console.log(e.message);
+    res.status(500).json({ message: 'Algo deu errado' });
+  }
 });
 
 app.get('/employees/:id', async (req, res) => {
