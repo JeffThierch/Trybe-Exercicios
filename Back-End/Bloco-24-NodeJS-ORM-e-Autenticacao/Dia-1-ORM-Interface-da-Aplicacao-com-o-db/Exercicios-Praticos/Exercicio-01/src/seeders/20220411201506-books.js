a6550d8ea607bed03886022a65bd6dd3b75f10f6
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2022-04-11 17:44:05 -0300

    :sparkles: Feat: Book Seeds

--- Back-End/Bloco-24-NodeJS-ORM-e-Autenticacao/Dia-1-ORM-Interface-da-Aplicacao-com-o-db/Exercicios-Praticos/Exercicio-01/src/seeders/20220411201506-books.js
+++ Back-End/Bloco-24-NodeJS-ORM-e-Autenticacao/Dia-1-ORM-Interface-da-Aplicacao-com-o-db/Exercicios-Praticos/Exercicio-01/src/seeders/20220411201506-books.js
@@ -1,27 +1,35 @@
 'use strict';
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up (queryInterface, _Sequelize) {
 
     await queryInterface.bulkInsert('Books', [
       {
         title: 'As 6 licoes',
         author: 'Mises',
-        pageQuantity: 100
+        page_quantity: 100,
+        created_at: new Date(Date.now()).toLocaleString('sv-SE'),
+        updated_at: new Date(Date.now()).toLocaleString('sv-SE'),
       },
       {
         title: 'Man the economy and state',
         author: 'Rothbard',
-        pageQuantity: 300
+        page_quantity: 300,
+        created_at: new Date(Date.now()).toLocaleString('sv-SE'),
+        updated_at: new Date(Date.now()).toLocaleString('sv-SE'),
       },
       {
         title: 'Acao humana',
         author: 'Mises',
-        pageQuantity: 800
+        page_quantity: 800,
+        created_at: new Date(Date.now()).toLocaleString('sv-SE'),
+        updated_at: new Date(Date.now()).toLocaleString('sv-SE'),
       },
       {
         title: 'Democracria o Deus que falhou',
         author: 'Hans',
+        created_at: new Date(Date.now()).toLocaleString('sv-SE'),
+        updated_at: new Date(Date.now()).toLocaleString('sv-SE'),
       }
     ], {})
   },
