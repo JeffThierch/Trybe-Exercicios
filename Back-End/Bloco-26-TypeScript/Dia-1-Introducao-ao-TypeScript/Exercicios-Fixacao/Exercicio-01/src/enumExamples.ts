c6d4513d724c6afa84243cdd91a1d1b27ddcfa1a
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2022-04-26 11:23:44 -0300

    Feat: Enuns Example

---
+++ Back-End/Bloco-26-TypeScript/Dia-1-Introducao-ao-TypeScript/Exercicios-Fixacao/Exercicio-01/src/enumExamples.ts
@@ -0,0 +1,35 @@
+enum daysOfWeek {
+  "Domingo" = 1,
+  "Segunda-Feira",
+  "Terca-Feira",
+  "Quarta Feira",
+  "Quinta Feira",
+  "Sexta Feira",
+  "Sabado"
+}
+
+enum rainbowColors {
+  firstColor = 'Vermelho',
+  secondColor = 'Laranja',
+  thirdColor = 'Amarela',
+  fouthColor = 'Verde',
+  fifthColor = 'Azul',
+  sixthColor = 'Azul-Escuro',
+  seventhColor = 'Violeta' 
+}
+
+enum interactionActions {
+  save,
+  print,
+  open,
+  view,
+  close
+}
+
+enum cardinalsRegions {
+  Norte = 'N',
+  Leste = 'L',
+  Sul = 'S',
+  Oeste = 'O'
+}
+
