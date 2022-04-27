29256179a5d2ed6fc42b56c2ab410bfa8a4004af
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2022-04-27 16:21:32 -0300

    Feat: getAll and create methods using class

--- Back-End/Bloco-26-TypeScript/Dia-2-Tipagem-Estatica-e-Generics/Exercicios-Fixacao/exercicio-02/src/models/Book.ts
+++ Back-End/Bloco-26-TypeScript/Dia-2-Tipagem-Estatica-e-Generics/Exercicios-Fixacao/exercicio-02/src/models/Book.ts
@@ -1,4 +1,12 @@
-import { Pool } from 'mysql2/promise';
+import { Pool, ResultSetHeader } from 'mysql2/promise';
+
+export interface Book {
+  id?: number,
+  title: string,
+  price: number,
+  author: string,
+  isbn: string
+}
 
 export default class BookModel {
   private connection: Pool;
@@ -7,8 +15,17 @@ export default class BookModel {
     this.connection = connection;
   }
 
-  public async getAll() {
+  public async getAll(): Promise<Book[]> {
     const [rows] = await this.connection.execute('SELECT * FROM Books');
-    return rows;
+    return rows as Book[];
+  }
+
+  public async create({title, price, author, isbn}: Book): Promise<Book> {
+    const [{insertId}] = await this.connection.execute<ResultSetHeader>(
+      'INSERT INTO books (title, price, author, isbn) VALUES (?, ?, ?, ?);', 
+      [title, price, author, isbn]
+    );
+
+    return {id: insertId, title, price, author, isbn};
   }
 }
