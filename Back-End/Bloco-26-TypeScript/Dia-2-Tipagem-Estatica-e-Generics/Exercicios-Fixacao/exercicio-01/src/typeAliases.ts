be9b722bb4d8033d0cb2ba57c3e6f0d9b4b920e3
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2022-04-27 15:37:28 -0300

    Feat: Exercicio-fixacao 01

---
+++ Back-End/Bloco-26-TypeScript/Dia-2-Tipagem-Estatica-e-Generics/Exercicios-Fixacao/exercicio-01/src/typeAliases.ts
@@ -0,0 +1,16 @@
+type Bird = {
+  color: string,
+  race: string,
+  name?: string,
+  age?: number
+}
+
+type Sum = (num1 : number, num2: number) => number;
+
+type Address = {
+  street: string,
+  number: number,
+  city: string,
+  state: string
+  country: string,
+}
