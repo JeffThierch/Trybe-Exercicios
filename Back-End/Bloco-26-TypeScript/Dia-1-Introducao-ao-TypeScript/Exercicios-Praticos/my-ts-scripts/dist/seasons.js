b36f9518b39773a54d785c74601533b10e0c2fff
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2022-04-27 13:56:40 -0300

    Feat: Exercicio 09

---
+++ Back-End/Bloco-26-TypeScript/Dia-1-Introducao-ao-TypeScript/Exercicios-Praticos/my-ts-scripts/dist/seasons.js
@@ -0,0 +1,16 @@
+"use strict";
+var YearMonths;
+(function (YearMonths) {
+    YearMonths[YearMonths["janeiro"] = 1] = "janeiro";
+    YearMonths[YearMonths["fevereiro"] = 2] = "fevereiro";
+    YearMonths[YearMonths["mar\u00E7o"] = 3] = "mar\u00E7o";
+    YearMonths[YearMonths["abril"] = 4] = "abril";
+    YearMonths[YearMonths["maio"] = 5] = "maio";
+    YearMonths[YearMonths["junho"] = 6] = "junho";
+    YearMonths[YearMonths["julho"] = 7] = "julho";
+    YearMonths[YearMonths["agosto"] = 8] = "agosto";
+    YearMonths[YearMonths["setembro"] = 9] = "setembro";
+    YearMonths[YearMonths["outubro"] = 10] = "outubro";
+    YearMonths[YearMonths["novembro"] = 11] = "novembro";
+    YearMonths[YearMonths["dezembro"] = 12] = "dezembro";
+})(YearMonths || (YearMonths = {}));
---
+++ Back-End/Bloco-26-TypeScript/Dia-1-Introducao-ao-TypeScript/Exercicios-Praticos/my-ts-scripts/src/seasons.ts
@@ -0,0 +1,14 @@
+enum YearMonths {
+  janeiro = 1,
+  fevereiro,
+  março,
+  abril,
+  maio,
+  junho,
+  julho,
+  agosto,
+  setembro,
+  outubro,
+  novembro,
+  dezembro,
+}
