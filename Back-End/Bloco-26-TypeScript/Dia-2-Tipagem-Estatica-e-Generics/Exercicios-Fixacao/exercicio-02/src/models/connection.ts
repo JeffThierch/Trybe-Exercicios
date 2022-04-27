6cff94b9ae59d9acc042cd74a5a55c5240f95e6f
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2022-04-27 16:25:44 -0300

    Feat: fix connection

--- Back-End/Bloco-26-TypeScript/Dia-2-Tipagem-Estatica-e-Generics/Exercicios-Fixacao/exercicio-02/src/models/connection.ts
+++ Back-End/Bloco-26-TypeScript/Dia-2-Tipagem-Estatica-e-Generics/Exercicios-Fixacao/exercicio-02/src/models/connection.ts
@@ -7,6 +7,7 @@ dotenv.config();
 export default mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
+  port: Number(process.env.DB_PORT),
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
 });
