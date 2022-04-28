63a8716d26cbc7046fff0df79a7b86dd1855f4f1
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2022-04-28 12:08:23 -0300

    Feat: Exercicio-Pratico-04

--- Back-End/Bloco-26-TypeScript/Dia-2-Tipagem-Estatica-e-Generics/Exercicios-Praticos/exercicio-01/src/Pizza.ts
+++ Back-End/Bloco-26-TypeScript/Dia-2-Tipagem-Estatica-e-Generics/Exercicios-Praticos/exercicio-01/src/Pizza.ts
@@ -1,12 +1,32 @@
 type Slices = 4 | 6 | 8
+type SugarSlices = 4
+
+type Common = 'Calabresa' | 'Frango' | 'Pepperoni'
+type Vegetarian = 'Marguerita' | 'Palmito' | 'Cogumelo'
+type Sugar = 'Nutela' | 'Goiabada com Queijo' | 'Marshmallow'
 
 interface IPizza {
   flavor: string;
   slices: Slices;
 }
 
+interface ICommonPizza extends IPizza {
+  flavor: Common;
+  slices: Slices;
+}
+
+interface IVegetarian extends IPizza {
+  flavor: Vegetarian;
+  slices: Slices;
+}
+
+interface ISugar extends IPizza {
+  flavor: Sugar;
+  slices: SugarSlices;
+}
 
 
+/* Exercicio 03
 const Calabresa: IPizza = {
   flavor: 'Calabresa',
   slices: 8
@@ -26,4 +46,47 @@ const Nutela: IPizza = {
   slices: 4
 }
 
-console.log(Nutela)
+console.log(Nutela) 
+*/
+
+const calabresa: ICommonPizza = {
+  flavor: "Calabresa",
+  slices: 6
+};
+
+console.log(calabresa);
+
+const frango: ICommonPizza = {
+  flavor: "Frango",
+  slices: 8
+};
+
+console.log(frango);
+
+const pepperoni: ICommonPizza = {
+  flavor: "Pepperoni",
+  slices: 4
+};
+
+console.log(pepperoni);
+
+const marguerita: IVegetarian = {
+   flavor: "Marguerita",
+   slices: 8
+};
+
+console.log(marguerita);
+
+const palmito: IVegetarian = {
+  flavor: "Palmito",
+  slices: 8
+};
+
+console.log(palmito);
+
+const nutella: ISugar = {
+  flavor: "Nutela",
+  slices: 4
+};
+
+console.log(nutella);
