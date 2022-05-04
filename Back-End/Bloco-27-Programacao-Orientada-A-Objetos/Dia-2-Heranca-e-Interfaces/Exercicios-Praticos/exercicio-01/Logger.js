bd3d4800cd17288efcbebd034517df7dca8fb6c9
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2022-05-04 18:59:19 -0300

    Feat: exercicio-fixacao-Logger-Composition

---
+++ Back-End/Bloco-27-Programacao-Orientada-A-Objetos/Dia-2-Heranca-e-Interfaces/Exercicios-Praticos/exercicio-01/Logger.js
@@ -0,0 +1,27 @@
+"use strict";
+class ConsoleLogger {
+    log(logString) {
+        console.log(logString, 'ConsoleLogger1');
+    }
+}
+class ConsoleLogger2 {
+    log(logString) {
+        console.log(logString, 'ConsoleLogger2');
+    }
+}
+class ExampleDatabase {
+    constructor(logger = new ConsoleLogger) {
+        this.logger = logger;
+    }
+    save(key, value) {
+        this.logger.log(`${key}: ${value}`);
+    }
+}
+const logger1 = new ConsoleLogger();
+const logger2 = new ConsoleLogger2();
+const db1 = new ExampleDatabase(logger1);
+const db2 = new ExampleDatabase(logger2);
+const db3 = new ExampleDatabase();
+db1.save('foo', "bar");
+db2.save('foo', "bar");
+db3.save('foo', "bar");
