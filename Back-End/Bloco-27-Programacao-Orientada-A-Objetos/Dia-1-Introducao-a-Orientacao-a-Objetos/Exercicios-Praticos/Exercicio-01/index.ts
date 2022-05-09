090b9e7b0f60b11ec76ddce2382e8446eccbd4ff
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2022-05-09 11:53:41 -0300

    Refac: Student class

--- Back-End/Bloco-27-Programacao-Orientada-A-Objetos/Dia-1-Introducao-a-Orientacao-a-Objetos/Exercicios-Praticos/Exercicio-01/index.ts
+++ Back-End/Bloco-27-Programacao-Orientada-A-Objetos/Dia-1-Introducao-a-Orientacao-a-Objetos/Exercicios-Praticos/Exercicio-01/index.ts
@@ -2,8 +2,26 @@ type TestScores = [number, number, number, number]
 type WorkScores = [number, number,]
 
 class Student {
-  registration: string;
-  name: string;
-  scores: TestScores;
-  workScores: WorkScores;
+  constructor(
+    private registration: string, 
+    public name: string, 
+    private scores: TestScores, 
+    private workScores: WorkScores) {}
+
+  get getRegistration() {
+    return this.registration;
+  }
+
+  sumScores(): number {
+    const allScores = [...this.scores, ...this.workScores];
+
+    return allScores.reduce((previousScore, currentScore) => ( previousScore + currentScore ), 0);
+  }
+
+  calcAverageScore(): number {
+    const quantityOfScores = (this.scores.length + this.workScores.length);
+    const totalScore = this.sumScores();
+
+    return totalScore / quantityOfScores;
+  }
 }
