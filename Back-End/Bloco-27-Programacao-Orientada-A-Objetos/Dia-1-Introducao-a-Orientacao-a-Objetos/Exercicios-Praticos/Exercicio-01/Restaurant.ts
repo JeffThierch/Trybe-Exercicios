33e272d56e6c62dbfef5dc0f645ea53ce4d15984
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2022-05-09 12:09:34 -0300

    Feat: Restaunt class

---
+++ Back-End/Bloco-27-Programacao-Orientada-A-Objetos/Dia-1-Introducao-a-Orientacao-a-Objetos/Exercicios-Praticos/Exercicio-01/Restaurant.ts
@@ -0,0 +1,31 @@
+class Client {
+  constructor(public name: string) {}
+}
+
+type PaymentForm = "Credit Card" | "Money" | "Debit Card"
+
+class Order {
+  
+  constructor(
+    public client: Client,
+    public consumedItems: Item[],
+    public paymentForm: PaymentForm,
+    public discountPorcent?: number,
+  ) {}
+
+  calcOrderTotal() {
+    return this.consumedItems.reduce((prevValue, actualValue) => (prevValue + actualValue.price), 0);
+  }
+
+  calcDiscountTotal() {
+    return (this.calcOrderTotal() * this.discountPorcent)
+  }
+
+  calcTotalWithDiscount() {
+    return this.calcOrderTotal() - this.calcDiscountTotal();
+  }
+}
+
+class Item {
+  constructor(public name: string, public price: number) {}
+}
