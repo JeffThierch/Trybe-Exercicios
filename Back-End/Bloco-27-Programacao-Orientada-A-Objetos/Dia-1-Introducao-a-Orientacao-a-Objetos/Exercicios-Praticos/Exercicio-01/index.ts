681b07eb05440b1bf8dfb69329cdde299bfc51f7
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2022-05-09 11:42:01 -0300

    Feat: Student class attributes

---
+++ Back-End/Bloco-27-Programacao-Orientada-A-Objetos/Dia-1-Introducao-a-Orientacao-a-Objetos/Exercicios-Praticos/Exercicio-01/index.ts
@@ -0,0 +1,9 @@
+type TestScores = [number, number, number, number]
+type WorkScores = [number, number,]
+
+class Student {
+  registration: string;
+  name: string;
+  scores: TestScores;
+  workScores: WorkScores;
+}
