5bf19c673493b6bb2e6ded96fac33016e2a4a5a0
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2022-05-09 15:09:00 -0300

    Refac: All classes

--- Back-End/Bloco-27-Programacao-Orientada-A-Objetos/Dia-1-Introducao-a-Orientacao-a-Objetos/Exercicios-Praticos/Exercicio-01/Restaurant.ts
+++ Back-End/Bloco-27-Programacao-Orientada-A-Objetos/Dia-1-Introducao-a-Orientacao-a-Objetos/Exercicios-Praticos/Exercicio-01/Restaurant.ts
@@ -1,5 +1,17 @@
 class Client {
-  constructor(public name: string) {}
+  constructor(private _name: string) {}
+
+  get name(): string {
+    return this._name;
+  }
+
+  set name(value: string) {
+    if(value.length < 3) {
+      throw new Error('O nome deve possuir no minimo 3 caracteres')
+    }
+
+    this._name = value;
+  }
 }
 
 type PaymentForm = "Credit Card" | "Money" | "Debit Card"
@@ -7,22 +19,58 @@ type PaymentForm = "Credit Card" | "Money" | "Debit Card"
 class Order {
   
   constructor(
-    public client: Client,
-    public consumedItems: Item[],
-    public paymentForm: PaymentForm,
-    public discountPorcent?: number,
+    private _client: Client,
+    private _consumedItems: Item[] = [],
+    private _paymentForm: PaymentForm,
+    private _discount: number = 0,
   ) {}
+
+  get client(): Client {
+    return this._client;
+  }
+
+  set client(value: Client) {
+    this._client = value
+  }
+
+  get consumedItems(): Item[] {
+    return this._consumedItems;
+  }
+
+  set consumedItems(value: Item[]) {
+    if(value.length === 0) {
+      throw new Error('O pedido deve ter pelo menos um item.');
+    }
+
+    this.consumedItems = value;
+  }
+
+  get paymentForm(): PaymentForm {
+    return this._paymentForm;
+  }
+
+  set paymentForm(value: PaymentForm) {
+    this._paymentForm = value
+  }
+
+  get discount(): number {
+    return this._discount;
+  }
+
+  set discount(value: number) {
+    if (value < 0) {
+      throw new Error('O disconto nao pode ser uma valor negativo.');
+    }
+
+    this._discount = value;
+  }
 
   calcOrderTotal() {
     return this.consumedItems.reduce((prevValue, actualValue) => (prevValue + actualValue.price), 0);
   }
 
   calcDiscountTotal() {
-    if(!this.discountPorcent) {
-      return 0
-    }
-    
-    return (this.calcOrderTotal() * this.discountPorcent)
+    return (this.calcOrderTotal() * this.discount)
   }
 
   calcTotalWithDiscount() {
@@ -31,5 +79,29 @@ class Order {
 }
 
 class Item {
-  constructor(public name: string, public price: number) {}
+  constructor(private _name: string, private _price: number) {}
+
+  get name(): string {
+    return this._name;
+  }
+
+  set name(value: string) {
+    if(value.length < 3) {
+      throw new Error('O nome deve possuir no minimo 3 caracteres');
+    }
+
+    this._name = value;
+  }
+
+  get price(): number {
+    return this._price
+  }
+
+  set price(value: number) {
+    if(value < 0) {
+      throw new Error('O preço deve ser positivo');
+    }
+
+    this._price = value;
+  }
 }
--- Back-End/Bloco-27-Programacao-Orientada-A-Objetos/Dia-1-Introducao-a-Orientacao-a-Objetos/Exercicios-Praticos/Exercicio-01/Student.ts
+++ Back-End/Bloco-27-Programacao-Orientada-A-Objetos/Dia-1-Introducao-a-Orientacao-a-Objetos/Exercicios-Praticos/Exercicio-01/Student.ts
@@ -1,26 +1,68 @@
-type TestScores = [number, number, number, number]
-type WorkScores = [number, number,]
-
 class Student {
-  constructor(
-    private registration: string, 
-    public name: string, 
-    private scores: TestScores, 
-    private workScores: WorkScores) {}
-
-  get getRegistration() {
-    return this.registration;
+    private _registration: string;
+    private _name: string; 
+    private _examsGrades: number[]; 
+    private _worksGrades: number[];
+  constructor(registration: string, name: string) {
+    this._registration = registration;
+    this._name = name;
+    this._examsGrades = [];
+    this._worksGrades = [];
   }
 
-  sumScores(): number {
-    const allScores = [...this.scores, ...this.workScores];
+  get registration() {
+    return this._registration;
+  }
+
+  set registration(value: string) {
+    this._registration = value
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value: string) {
+    if(value.length < 3) {
+      throw new Error('O nome deve conter no minimo 3 caracteres');
+    }
+
+    this._name = value;
+  }
+
+  get examsGrades(): number[] {
+    return this._examsGrades;
+  }
+
+  set examsGrades(value: number[]) {
+    if(value.length > 4) {
+      throw new Error('A pessoa estudante so pode possuir 4 notas de provas.');
+    }
+    
+    this._examsGrades = value;
+  }
+
+  get worksGrades(): number[] {
+    return this._examsGrades;
+  }
+
+  set worksGrades(value: number[]) {
+    if(value.length > 2) {
+      throw new Error('A pessoa estudante so pode possuir 2 notas de trabalhos.');
+    }
+
+    this._examsGrades = value;
+  }
+
+  sumGrades(): number {
+    const allScores = [...this._examsGrades, ...this._worksGrades];
 
     return allScores.reduce((previousScore, currentScore) => ( previousScore + currentScore ), 0);
   }
 
-  calcAverageScore(): number {
-    const quantityOfScores = (this.scores.length + this.workScores.length);
-    const totalScore = this.sumScores();
+  calcAverageGrades(): number {
+    const quantityOfScores = (this._examsGrades.length + this._worksGrades.length);
+    const totalScore = this.sumGrades();
 
     return totalScore / quantityOfScores;
   }
