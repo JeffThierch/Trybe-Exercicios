be207a78303bc6a17adc77f7f0b32c24a1ad9fb9
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2022-05-19 13:47:56 -0300

    Feat: removePlantsByIdController

--- Back-End/Bloco-27-Programacao-Orientada-A-Objetos/Dia-4-SOLID-Introducao-e-Principios-S-O-e-D/Exercicios-Praticos/exercicio-01/src/useCases/removePlantById/RemovePlantByIdController.ts.ts
+++ Back-End/Bloco-27-Programacao-Orientada-A-Objetos/Dia-4-SOLID-Introducao-e-Principios-S-O-e-D/Exercicios-Praticos/exercicio-01/src/useCases/removePlantById/RemovePlantByIdController.ts.ts
@@ -1,10 +1,15 @@
 import { NextFunction, Request, Response } from "express";
+import { RemovePlantByIdUseCase } from "./RemovePlantByIdUseCase";
 
 export class RemovePlantByIdController {
-  constructor() {}
+  constructor(private removePlantsByIdUseCase: RemovePlantByIdUseCase) {}
   async handle(request: Request, response: Response, next: NextFunction) {
     try {
-      
+      const { id } = request.params;
+
+      const plant = await this.removePlantsByIdUseCase.execute(id)
+
+      return response.status(200).json(plant)
     } catch (error) {
       next(error)
     }
