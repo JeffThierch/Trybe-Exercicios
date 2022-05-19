bb0e7d06dccce5ed06bf04f874f332b66bff6181
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2022-05-19 13:56:05 -0300

    Feat: EditPlantController

--- Back-End/Bloco-27-Programacao-Orientada-A-Objetos/Dia-4-SOLID-Introducao-e-Principios-S-O-e-D/Exercicios-Praticos/exercicio-01/src/useCases/editPlant/EditPlantController.ts
+++ Back-End/Bloco-27-Programacao-Orientada-A-Objetos/Dia-4-SOLID-Introducao-e-Principios-S-O-e-D/Exercicios-Praticos/exercicio-01/src/useCases/editPlant/EditPlantController.ts
@@ -1,10 +1,18 @@
 import { NextFunction, Request, Response } from "express";
+import { IPlant } from "../../../interfaces/IPlant";
+import { EditPlantUseCase } from "./EditPlantUseCase";
 
 export class EditPlantController {
-  constructor() {}
+  constructor(private editPlantUseCase: EditPlantUseCase) {}
+
   async handle(request: Request, response: Response, next: NextFunction) {
     try {
-      
+      const { breed, needsSun, origin, size, specialCare } = request.body as IPlant
+      const { id } = request.params;
+
+      const newPlant = await this.editPlantUseCase.execute(id, {breed, needsSun, origin, size, specialCare})
+
+      return response.status(200).json(newPlant)
     } catch (error) {
       next(error)
     }
