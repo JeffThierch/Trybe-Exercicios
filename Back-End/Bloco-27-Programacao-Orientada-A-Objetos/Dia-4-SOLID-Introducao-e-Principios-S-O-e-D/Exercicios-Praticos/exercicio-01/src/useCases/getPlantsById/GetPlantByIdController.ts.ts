936d2134a5913a98907fabc87a5840f6cbeff298
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2022-05-19 13:44:10 -0300

    Feat: Basic controllers structure

--- Back-End/Bloco-27-Programacao-Orientada-A-Objetos/Dia-4-SOLID-Introducao-e-Principios-S-O-e-D/Exercicios-Praticos/exercicio-01/src/useCases/getPlantsById/GetPlantByIdController.ts.ts
+++ Back-End/Bloco-27-Programacao-Orientada-A-Objetos/Dia-4-SOLID-Introducao-e-Principios-S-O-e-D/Exercicios-Praticos/exercicio-01/src/useCases/getPlantsById/GetPlantByIdController.ts.ts
@@ -1,4 +1,12 @@
+import { NextFunction, Request, Response } from "express";
+
 export class GetPlantsByIdController {
   constructor() {}
-  async handle() {}
+  async handle(request: Request, response: Response, next: NextFunction) {
+    try {
+      
+    } catch (error) {
+      next(error)
+    }
+  }
 }
