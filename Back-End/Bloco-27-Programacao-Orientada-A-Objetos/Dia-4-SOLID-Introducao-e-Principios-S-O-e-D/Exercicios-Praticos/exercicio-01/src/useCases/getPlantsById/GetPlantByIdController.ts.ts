ddd1970bffa92621c9ac7dc0dfae1561dd95c9ed
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2022-05-19 13:56:21 -0300

    Feat: GetPlantByIdController

--- Back-End/Bloco-27-Programacao-Orientada-A-Objetos/Dia-4-SOLID-Introducao-e-Principios-S-O-e-D/Exercicios-Praticos/exercicio-01/src/useCases/getPlantsById/GetPlantByIdController.ts.ts
+++ Back-End/Bloco-27-Programacao-Orientada-A-Objetos/Dia-4-SOLID-Introducao-e-Principios-S-O-e-D/Exercicios-Praticos/exercicio-01/src/useCases/getPlantsById/GetPlantByIdController.ts.ts
@@ -1,10 +1,15 @@
 import { NextFunction, Request, Response } from "express";
+import { GetPlantsByIdUseCase } from "./GetPlantByIdUseCase";
 
 export class GetPlantsByIdController {
-  constructor() {}
+  constructor(private getPlantsByIdUseCase: GetPlantsByIdUseCase) {}
   async handle(request: Request, response: Response, next: NextFunction) {
     try {
-      
+      const { id } = request.params;
+
+      const plant = await this.getPlantsByIdUseCase.execute(id)
+
+      return response.status(200).json(plant)
     } catch (error) {
       next(error)
     }
