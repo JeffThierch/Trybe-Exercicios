d4dfaf9d1d0531952d7f942859eccd2ca4027a7b
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2022-05-19 13:45:59 -0300

    Feat: getPlantsController

--- Back-End/Bloco-27-Programacao-Orientada-A-Objetos/Dia-4-SOLID-Introducao-e-Principios-S-O-e-D/Exercicios-Praticos/exercicio-01/src/useCases/getPlants/GetPlantsController.ts
+++ Back-End/Bloco-27-Programacao-Orientada-A-Objetos/Dia-4-SOLID-Introducao-e-Principios-S-O-e-D/Exercicios-Praticos/exercicio-01/src/useCases/getPlants/GetPlantsController.ts
@@ -1,10 +1,14 @@
 import { NextFunction, Request, Response } from "express";
+import { GetPlantsUseCase } from "./GetPlantsUseCase";
 
 export class GetPlantsController {
-  constructor() {}
-  async handle(request: Request, response: Response, next: NextFunction) {
+  constructor(private getPlantsUseCase: GetPlantsUseCase) {}
+
+  async handle(_request: Request, response: Response, next: NextFunction) {
     try {
-      
+      const plants = await this.getPlantsUseCase.execute()
+
+      return response.status(200).json(plants);
     } catch (error) {
       next(error)
     }
