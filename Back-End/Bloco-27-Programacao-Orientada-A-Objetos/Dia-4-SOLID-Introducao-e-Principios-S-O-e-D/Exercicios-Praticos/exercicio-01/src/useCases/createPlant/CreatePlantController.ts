bf9f9835ddd7734777e119cb4d2515584657b98e
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2022-05-19 13:55:56 -0300

    Feat: CreatePlantsController

--- Back-End/Bloco-27-Programacao-Orientada-A-Objetos/Dia-4-SOLID-Introducao-e-Principios-S-O-e-D/Exercicios-Praticos/exercicio-01/src/useCases/createPlant/CreatePlantController.ts
+++ Back-End/Bloco-27-Programacao-Orientada-A-Objetos/Dia-4-SOLID-Introducao-e-Principios-S-O-e-D/Exercicios-Praticos/exercicio-01/src/useCases/createPlant/CreatePlantController.ts
@@ -1,10 +1,17 @@
 import { NextFunction, Request, Response } from "express";
+import { IPlant } from "../../../interfaces/IPlant";
+import { CreatePlantUseCase } from "./CreatePlantUseCase";
 
 export class CreatePlantController {
-  constructor() {}
+  constructor(private createPlantUseCase: CreatePlantUseCase) {}
+
   async handle(request: Request, response: Response, next: NextFunction) {
     try {
-      
+      const { breed, needsSun, origin, size, specialCare } = request.body as IPlant
+
+      const newPlant = await this.createPlantUseCase.execute({breed, needsSun, origin, size, specialCare})
+
+      return response.status(201).json(newPlant)
     } catch (error) {
       next(error)
     }
