f5de02d304d15dfa3755be7cb6fff7d9a4de2087
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2022-05-19 14:09:02 -0300

    Feat: plants route

--- Back-End/Bloco-27-Programacao-Orientada-A-Objetos/Dia-4-SOLID-Introducao-e-Principios-S-O-e-D/Exercicios-Praticos/exercicio-01/src/routes/plantsRoute.ts
+++ Back-End/Bloco-27-Programacao-Orientada-A-Objetos/Dia-4-SOLID-Introducao-e-Principios-S-O-e-D/Exercicios-Praticos/exercicio-01/src/routes/plantsRoute.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
+import { getPlantsController } from '../useCases/getPlants';
 
 const router = Router();
 
 router
-  .get('/');
+  .get('/', getPlantsController.handle);
 
 export default router;
