e998c73fd7e5c5300cf272b9f87ae3d45ecd6a06
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2022-05-19 14:09:30 -0300

    Feat: sunny routes

--- Back-End/Bloco-27-Programacao-Orientada-A-Objetos/Dia-4-SOLID-Introducao-e-Principios-S-O-e-D/Exercicios-Praticos/exercicio-01/src/routes/sunnyRoute.ts
+++ Back-End/Bloco-27-Programacao-Orientada-A-Objetos/Dia-4-SOLID-Introducao-e-Principios-S-O-e-D/Exercicios-Praticos/exercicio-01/src/routes/sunnyRoute.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
+import { getPlantsThatNeedSunWithIdController } from '../useCases/getPlantsThatNeedSunWithId';
 
 const router = Router();
 
 router
-  .get('/:id');
+  .get('/:id', getPlantsThatNeedSunWithIdController.handle);
 
 export default router;
