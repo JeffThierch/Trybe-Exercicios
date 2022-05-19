13b14913d1dee3a87be324fe93f84d59f04df895
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2022-05-19 14:08:29 -0300

    Feat: plant routes

--- Back-End/Bloco-27-Programacao-Orientada-A-Objetos/Dia-4-SOLID-Introducao-e-Principios-S-O-e-D/Exercicios-Praticos/exercicio-01/src/routes/plantRoutes.ts
+++ Back-End/Bloco-27-Programacao-Orientada-A-Objetos/Dia-4-SOLID-Introducao-e-Principios-S-O-e-D/Exercicios-Praticos/exercicio-01/src/routes/plantRoutes.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express';
+import { createPlantController } from '../useCases/createPlant';
+import { editPlantController } from '../useCases/editPlant';
+import { getPlantByIdController } from '../useCases/getPlantsById';
+import { removePlantByIdController } from '../useCases/removePlantById';
 
 const router = Router();
 
 router
-  .post('/')
-  .get('/:id')
-  .delete('/:id')
-  .post('/:id');
+  .post('/', createPlantController.handle)
+  .get('/:id', getPlantByIdController.handle)
+  .delete('/:id', removePlantByIdController.handle)
+  .post('/:id', editPlantController.handle);
 
 export default router;
