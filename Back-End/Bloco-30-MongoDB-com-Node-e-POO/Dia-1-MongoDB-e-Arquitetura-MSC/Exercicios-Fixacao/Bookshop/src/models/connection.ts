33026c65055dfe4c41dd07d6d6cf1beb724a9de3
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2022-06-25 16:24:58 -0300

    Feat: Exericios-Fixacao

---
+++ Back-End/Bloco-30-MongoDB-com-Node-e-POO/Dia-1-MongoDB-e-Arquitetura-MSC/Exercicios-Fixacao/Bookshop/src/models/connection.ts
@@ -0,0 +1,9 @@
+// models/connection.ts
+
+import mongoose from "mongoose";
+
+const connection = (
+  mongoDatabaseURI = "mongodb://localhost:27017/model_example"
+) => mongoose.connect(mongoDatabaseURI);
+
+export default connection;
